8f8f53fa05e54b0151f1db820a3caed5229ab5f7
timnh <timnh@PC2CYEVA>
2024-09-20 13:38:42 +0200

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Skriv in ett nummer");
+
+int number;
+while (!int.TryParse(Console.ReadLine(), out number)) //checking if a person wrote a number or not. repeat until a number is written
+{
+    Console.WriteLine("Skriv in en siffra eller ett tal, tack!");
+}
+
+if (number < 10) //checks if the number is less than 10
+{
+    Console.WriteLine("Oj, vad lågt tal!");
+}
+else //if its not less than 10
+{
+    Console.WriteLine("Stort Tal!");
+}
+
+Console.WriteLine("Vad heter du?");
+var name = Console.ReadLine();
+Console.WriteLine($"Hej {name}!");
+
+for (int i = 0; i <= number; i++) //writing out all number up to the number the person chose in the beginning
+{
+    Console.WriteLine(i);
+}
